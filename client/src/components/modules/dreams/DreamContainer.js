ec6df7f433f270faf43c67a14115d6894dc886ba
Angela Zhao <angelazhao@dhcp-10-31-127-231.dyn.mit.edu>
2022-01-13 16:57:24 -0500

    minor progress on feed css

--- client/src/components/modules/dreams/DreamContainer.js
+++ client/src/components/modules/dreams/DreamContainer.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
+import "./DreamContainer.css";
 
-const DreamContainer =  (props) => {
-    /* TODO: make pretty */
-    return (
-        <div>
-            <p>{props.date}</p>
-            <strong>{props.name}</strong>: <p>{props.content}</p>
-        </div>
-    );
+const DreamContainer = (props) => {
+  /* TODO: confused on how to get individual bubbles?*/
+  return (
+    <div className="container">
+      <p className="date">{props.date}</p>
+      <strong className="name">{props.name}</strong> <p className="content">{props.content}</p>
+    </div>
+  );
 };
 
+/* TODO: function to get date in mm/dd/yyyy format?? how to apply*/
+function convertDate(date) {
+  date = preg_split("[-], date");
+  date = date[1] + "/" + date[2] + "/" + date[0];
+}
 export default DreamContainer;

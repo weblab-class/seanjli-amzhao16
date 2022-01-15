f7eab013f872eb3d8867a6f884d8aa53054113f6
Angela Zhao <angelazhao@dhcp-10-31-127-231.dyn.mit.edu>
2022-01-15 16:39:33 -0500

    write dream css

--- client/src/components/pages/WriteDream.js
+++ client/src/components/pages/WriteDream.js
@@ -5,6 +5,7 @@ import SubmitDreamButton from "../modules/dreams/SubmitDreamButton.js";
 import SubmitDream from "../modules/dreams/SubmitDream.js";
 import "./WriteDream.css";
 import NavBar from "../modules/NavBar.js";
+import moment from "moment";
 
 const WriteDream = (props) => {
   const [value, setValue] = useState("");
@@ -12,15 +13,26 @@ const WriteDream = (props) => {
   const handleChange = (event) => {
     setValue(event.target.value);
   };
+  // const currentTime = moment().local();
+  const currHour = parseInt(moment().format().substring(11, 13));
+
   return (
-    <>
+    <div>
       <NavBar type="w" handleLogout={props.handleLogout} />
-      <br />
-      <br />
-      <br />
-      {/* <SubmitDream /> */}
-      <SubmitDream />
-    </>
+      <div className="writing">
+        <div className="writeTitle">
+          {currHour < 12 ? (
+            <em className="greeting">good morning!</em>
+          ) : currHour < 18 ? (
+            <em className="greeting">good afternoon!</em>
+          ) : (
+            <em className="greeting">good evening!</em>
+          )}
+          <em className="currentDate">{moment().format("ll")}</em>
+        </div>
+        <SubmitDream />
+      </div>
+    </div>
   );
 };
 

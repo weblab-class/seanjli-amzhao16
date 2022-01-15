a60cbe0c28b9ff4f56a710cdb3de409405229e75
Angela Zhao <angelazhao@dhcp-10-31-127-231.dyn.mit.edu>
2022-01-14 21:06:55 -0500

    google client id change

--- client/src/components/modules/dreams/DreamContainer.js
+++ client/src/components/modules/dreams/DreamContainer.js
@@ -5,7 +5,7 @@ const DreamContainer = (props) => {
   /* TODO: confused on how to get individual bubbles?*/
   return (
     <div className="container">
-      <p className="date">{props.date}</p>
+      <p className="date">{convertDate(props.date)}</p>
       <strong className="name">{props.name}</strong> <p className="content">{props.content}</p>
     </div>
   );
@@ -13,8 +13,18 @@ const DreamContainer = (props) => {
 
 /* TODO: function to get date in mm/dd/yyyy format?? how to apply*/
 function convertDate(date) {
-  dateChunks = preg_split("[-]", date);
-  dateNew = dateChunks[1] + "/" + dateChunks[2] + "/" + dateChunks[0];
-  return dateNew;
+  let dateNew = date.substring(5, 7) + "/" + date.substring(8, 10) + "/" + date.substring(0, 4);
+  let hour = parseInt(date.substring(11, 13));
+  let newTime = "";
+  if (hour > 12) {
+    hour = hour - 12;
+    newTime = hour.toString() + date.substring(13, 19) + " PM";
+  } else if (hour > 9) {
+    newTime = hour.toString() + date.substring(13, 19) + " AM";
+  } else {
+    hour = parseInt(date.substring(12, 13));
+    newTime = hour.toString() + date.substring(13, 19) + " AM";
+  }
+  return "Posted on " + dateNew + " at " + newTime;
 }
 export default DreamContainer;

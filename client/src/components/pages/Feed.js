09834b4f46d374a6d77b7bb99c6bf94d0b349bad
Angela Zhao <angelazhao@dhcp-10-31-127-231.dyn.mit.edu>
2022-01-17 02:08:02 -0500

    some of feed css

--- client/src/components/pages/Feed.js
+++ client/src/components/pages/Feed.js
@@ -3,8 +3,14 @@
 import React, { useState, useEffect } from "react";
 import DreamContainer from "../modules/dreams/DreamContainer.js";
 import NavBar from "../modules/NavBar.js";
-
+import "./Feed.css";
 import { get, post } from "../../utilities.js";
+import { useParams } from "@reach/router";
+
+import FriendList from "../modules/friendsystem/FriendList";
+import MakeFriendRequests from "../modules/friendsystem/MakeFriendRequests";
+import AcceptFriendRequests from "../modules/friendsystem/AcceptFriendRequests";
+import OutgoingFriendRequests from "../modules/friendsystem/OutgoingFriendRequests";
 
 const Feed = (props) => {
   const [dreams, setDreams] = useState([]);
@@ -24,14 +30,81 @@ const Feed = (props) => {
     get("/api/dreams", { parent: [...friends, props.userId] }).then((x) => setDreams(x.reverse()));
   }, [friends]);
 
+  const [users, setUsers] = useState([]);
+  const [requests, setRequests] = useState([]);
+  const [outgoing, setOutgoing] = useState([]);
+  const [requestees, setRequestees] = useState([]);
+
+  useEffect(() => {
+    get("/api/getUser").then((x) => setUsers(x));
+    get("/api/getMe").then((x) => setMe(x));
+    get("/api/getFriendRequest").then((x) => setRequests(x));
+    get("/api/getOutgoingFriendRequest").then((x) => setOutgoing(x));
+  }, []);
+
+  useEffect(() => {
+    setFriends(me[0].friends);
+  }, [me]);
+
+  useEffect(() => {
+    setRequestees(
+      users.filter(
+        (x) =>
+          !(
+            friends.includes(x._id) ||
+            requests.map((x) => x.sender_id).includes(x._id) ||
+            outgoing.map((x) => x.recipient_id).includes(x._id) ||
+            x._id === props.userId
+          )
+      )
+    );
+  }, [users, friends, requests, outgoing]);
+
   return (
     <div>
       <NavBar type="f" handleLogout={props.handleLogout} userId={props.userId} />
-      <br />
-      <br />
-      {dreams.map((dream) => (
-        <DreamContainer date={dream.timeStamp} name={dream.author.name} content={dream.content} />
-      ))}
+      <div className="friendsSectionContainer">
+        <div>
+          <div className="myFriendsLabel">my friends</div>
+          <div className="myFriendsArea">
+            <FriendList
+              users={users}
+              requestees={requestees}
+              setRequestees={setRequestees}
+              friends={friends}
+              setFriends={setFriends}
+            />
+          </div>
+        </div>
+        <div>
+          <div className="incomingRequestsArea">
+            <AcceptFriendRequests
+              requests={requests}
+              setRequests={setRequests}
+              friends={friends}
+              setFriends={setFriends}
+            />
+          </div>
+          <div className="incomingRequestsLabel">incoming requests</div>
+        </div>
+        <div>
+          <div className="outgoingRequestsArea">
+            <OutgoingFriendRequests outgoing={outgoing} setOutgoing={setOutgoing} />
+          </div>
+          <div className="outgoingRequestsLabel">outgoing requests</div>
+        </div>
+      </div>
+      <div className="friendsContainerBorder">
+        <div className="friendsDreamsContainer">
+          {dreams.map((dream) => (
+            <DreamContainer
+              date={dream.timeStamp}
+              name={dream.author.name}
+              content={dream.content}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };

bdfed101db9825261252c96c96f315d4d0053476
Angela Zhao <angelazhao@dhcp-10-31-127-231.dyn.mit.edu>
2022-01-21 01:16:30 -0500

    not found page

--- client/src/components/pages/NotFound.js
+++ client/src/components/pages/NotFound.js
@@ -1,10 +1,19 @@
 import React from "react";
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <p>The page you requested couldn't be found.</p>
+    <div className="notFoundContainer">
+      <h1 className="ohNo">oh no!</h1>
+      <p className="dneNotFound">
+        this page doesn't seem to exist. try going to the following link and logging in
+      </p>
+      <div className="itWontCenterTheLink">
+        <a className="dneNotFound" href="https://dream-logbook.herokuapp.com">
+          {" "}
+          https://dream-logbook.herokuapp.com
+        </a>
+      </div>
     </div>
   );
 };

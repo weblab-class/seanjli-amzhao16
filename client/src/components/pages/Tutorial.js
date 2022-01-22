4c26ae35f1b03a9879d53abd65969235c6aeaf05
Sean Li <seanjli@dhcp-10-29-50-60.dyn.mit.edu>
2022-01-22 00:48:24 -0500

    added pages to tutorial

--- client/src/components/pages/Tutorial.js
+++ client/src/components/pages/Tutorial.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./Tutorial.css";
 
 const Tutorial = (props) => {
+
+  const [page, setPage] = useState(0);
+
+  const pages = [
+    <div>hi</div>,
+    <div>hi but better</div>,
+    <div>rawr hi angela</div>,
+    <div>bonk</div>,
+    <div>more bonk</div>,
+    <div>im bored</div>,
+    <div>help</div>,
+    <div>fin</div>
+  ]
+
+  const nextPage = () => {
+    setPage(page + 1);
+  }
+
+  const prevPage = () => {
+    setPage(page - 1);
+  }
+
   return (
     <div>
       <a className="exitTutorial" href="/">
         exit
       </a>
-      <div className="tutorialContainer"></div>
+      <div className="tutorialContainer">
+        {pages[page]}
+        {(page > 0) ? <button onClick={prevPage}>previous page</button> : <></>}
+        {(page < pages.length - 1) ? <button onClick={nextPage}>next page</button> : <></>}
+
+      </div>
     </div>
   );
 };

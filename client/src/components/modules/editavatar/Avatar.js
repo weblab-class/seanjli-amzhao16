407afcbe77371e67f6e518b2b12c2f7a58a56f3f
Angela Zhao <angelazhao@dhcp-10-31-127-231.dyn.mit.edu>
2022-01-23 01:16:38 -0500

    added accessories wahoo

--- client/src/components/modules/editavatar/Avatar.js
+++ client/src/components/modules/editavatar/Avatar.js
@@ -2,12 +2,18 @@ import "../../pages/Profile.css";
 import "./AvatarSkin.css";
 import "./AvatarShirts.css";
 import "./AvatarHair.css";
+import "./AvatarGlasses.css";
+import "./AvatarNeckwear.css";
+import "./AvatarHat.css";
 import React from "react";
 
 const Avatar = (props) => {
   return (
     <div>
       <div className={"skin-" + props.avatar.skin}></div>
+      <div className={"glasses-" + props.avatar.glasses}></div>
+      <div className={"hat-" + props.avatar.hat}></div>
+      <div className={"neck-" + props.avatar.neck}></div>
       <div className={"shirts-" + props.avatar.shirt}></div>
       <div className={props.avatar.hairType + "-" + props.avatar.hairColor + "-hair"}></div>
     </div>
